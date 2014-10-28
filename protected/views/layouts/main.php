cb1e41364cbf28cbb6eb012e893490f412cf426f
jlgg <virgoo.07.jlgg@gmail.com>
2014-10-27 22:35:32 -0600

    Se acomoda la imágen de inicio

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -219,8 +219,8 @@ input.gsc-search-button {
 										<table width="911" border="0" align="center" cellpadding="0"
 											cellspacing="0">
 											<tr>
-												<td width="591" bgcolor="#C2BDA0"><?php echo CHtml::link(CHtml::image("http://www.biodiversidad.gob.mx/usos/alimentacion/images/titPeces_02.png", "Aguacate", array("width"=>40, "height"=>301, "border"=>0, "id"=>"Aguacate", "name"=>"Aguacate")), Yii::app()->request->baseUrl); ?>
-													
+												<td width="591" bgcolor="#C2BDA0">
+												<?php echo CHtml::link(CHtml::image("http://www.biodiversidad.gob.mx/usos/alimentacion/images/titPeces_02.png", "Aguacate", array("width"=>40, "height"=>301, "border"=>0, "id"=>"Aguacate", "name"=>"Aguacate", "style"=>"position:absolute;")), Yii::app()->request->baseUrl); ?>
 													<?php echo CHtml::link(CHtml::image("http://www.biodiversidad.gob.mx/usos/alimentacion/images/imgPeces.png", "Peces", array("width"=>547, "height"=>301)), Yii::app()->request->baseUrl); ?>
 													</td>
 												<td width="320" bgcolor="#D2CEB9"><?php 
