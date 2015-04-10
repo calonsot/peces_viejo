045e26daae77cee507e39e28f9b3806bb9e6d3a0
calonso-conabio <calonso@conabio.gob.mx>
2015-04-10 14:15:50 -0500

    guarda la seleccion del mapa en el buscador

--- assets/js/buscador.js
+++ assets/js/buscador.js
@@ -2,11 +2,39 @@
  * Javascript del buscador
  */
 
-function filtros(filtro, accion)
+function MM_swapImgRestore() 
+{ //v3.0
+  var i,x,a=document.MM_sr; for(i=0;a&&i<a.length&&(x=a[i])&&x.oSrc;i++) x.src=x.oSrc;
+}
+
+function MM_preloadImages() 
+{ //v3.0
+  var d=document; if(d.images){ if(!d.MM_p) d.MM_p=new Array();
+    var i,j=d.MM_p.length,a=MM_preloadImages.arguments; for(i=0; i<a.length; i++)
+    if (a[i].indexOf("#")!=0){ d.MM_p[j]=new Image; d.MM_p[j++].src=a[i];}}
+}
+
+function MM_findObj(n, d) 
+{ //v4.01
+  var p,i,x;  if(!d) d=document; if((p=n.indexOf("?"))>0&&parent.frames.length) {
+    d=parent.frames[n.substring(p+1)].document; n=n.substring(0,p);}
+  if(!(x=d[n])&&d.all) x=d.all[n]; for (i=0;!x&&i<d.forms.length;i++) x=d.forms[i][n];
+  for(i=0;!x&&d.layers&&i<d.layers.length;i++) x=MM_findObj(n,d.layers[i].document);
+  if(!x && d.getElementById) x=d.getElementById(n); return x;
+}
+
+function MM_swapImage() 
+{ //v3.0
+	//alert(1);
+  var i,j=0,x,a=MM_swapImage.arguments; document.MM_sr=new Array; for(i=0;i<(a.length-2);i+=3)
+   if ((x=MM_findObj(a[i]))!=null){document.MM_sr[j++]=x; if(!x.oSrc) x.oSrc=x.src; x.src=a[i+2];}
+}
+
+function filtros(filtro, accion, es_zona)
 {
 	var values = {};
 	values['accion'] = accion;
-	
+		
 	switch(filtro.attr('id'))
 	{
 	    // Para todos los textos o valores unicos
@@ -109,19 +137,35 @@ function filtros(filtro, accion)
         		if (html_json.buscador_zona != "")
         		{
         			if (html_json.buscador_zona == '1')
-        				$('#buscador_zona1').prop('checked', true); 	
+        			{
+        				$('#buscador_zona1').prop('checked', true);
+        				MM_swapImage('mapaz','','http://172.16.3.222/peces/imagenes/aplicacion/zonas_pesqueras_mapap_i.jpg',1);
+        			}
         			if (html_json.buscador_zona == '2')
+        			{
         				$('#buscador_zona2').prop('checked', true);
+        				MM_swapImage('mapaz','','http://172.16.3.222/peces/imagenes/aplicacion/zonas_pesqueras_mapap_ii.jpg',1);
+        			}
         			if (html_json.buscador_zona == '3')
+        			{
         				$('#buscador_zona3').prop('checked', true);
+        				MM_swapImage('mapaz','','http://172.16.3.222/peces/imagenes/aplicacion/zonas_pesqueras_mapap_iii.jpg',1);
+        			}
         			if (html_json.buscador_zona == '4')
+        			{
         				$('#buscador_zona4').prop('checked', true);
+        				MM_swapImage('mapaz','','http://172.16.3.222/peces/imagenes/aplicacion/zonas_pesqueras_mapag_i.jpg',1);
+        			}
         			if (html_json.buscador_zona == '5')
+        			{
         				$('#buscador_zona5').prop('checked', true);
+        				MM_swapImage('mapaz','','http://172.16.3.222/peces/imagenes/aplicacion/zonas_pesqueras_mapag_ii.jpg',1);
+        			}
         			if (html_json.buscador_zona == '6')
+        			{
         				$('#buscador_zona6').prop('checked', true);
-        			if (html_json.buscador_zona == '7')
-        				$('#buscador_zona7').prop('checked', true);
+        				MM_swapImage('mapaz','','http://172.16.3.222/peces/imagenes/aplicacion/zonas_pesqueras_mapag_iii.jpg',1);
+        			}
         		}
         	}
         },
@@ -164,9 +208,9 @@ $(document).ready(function(){
 		$("#dresul_"+id).addClass( "ver" );
 	});
 	
-	$("[id^='zona']").on('click', function(){
+	$("[id^='zona']").on('click', function() {
 		$('#buscador_'+$(this).attr('id')).prop('checked', true);
-		filtros($(this), 'guarda');
+		filtros($('#buscador_'+$(this).attr('id')), 'guarda');
 		$('#buscador').submit();
 	});
 });
