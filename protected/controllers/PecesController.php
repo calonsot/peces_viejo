b5e3237e51b8260dde2ff0a1546c8dcc3baf1c6f
calonso-conabio <calonso@conabio.gob.mx>
2017-05-15 13:57:59 -0500

    los archivos se generaron de nuevo, para la tabla plana

--- protected/controllers/PecesController.php
+++ protected/controllers/PecesController.php
@@ -1,4 +1,5 @@
 <?php
+
 class PecesController extends Controller
 {
 	/**
@@ -6,16 +7,18 @@ class PecesController extends Controller
 	 * using two-column layout. See 'protected/views/layouts/column2.php'.
 	 */
 	public $layout='//layouts/column2';
+
 	/**
 	 * @return array action filters
 	 */
 	public function filters()
 	{
 		return array(
-				'accessControl', // perform access control for CRUD operations
-				'postOnly + delete', // we only allow deletion via POST request
+			'accessControl', // perform access control for CRUD operations
+			'postOnly + delete', // we only allow deletion via POST request
 		);
 	}
+
 	/**
 	 * Specifies the access control rules.
 	 * This method is used by the 'accessControl' filter.
@@ -30,8 +33,8 @@ class PecesController extends Controller
 				),
 				/*array('allow', // allow authenticated user to perform 'create' and 'update' actions
 				 'actions'=>array('create','update'),
-						'users'=>array('@'),
-				),*/
+				 'users'=>array('@'),
+				 ),*/
 				array('allow', // allow admin user to perform 'admin' and 'delete' actions
 						'actions'=>array('admin','delete','create','update'),
 						'users'=>array('admin'),
@@ -41,6 +44,7 @@ class PecesController extends Controller
 				),
 		);
 	}
+
 	/**
 	 * Displays a particular model.
 	 * @param integer $id the ID of the model to be displayed
@@ -48,9 +52,10 @@ class PecesController extends Controller
 	public function actionView($id)
 	{
 		$this->render('view',array(
-				'model'=>$this->loadModel($id),
+			'model'=>$this->loadModel($id),
 		));
 	}
+
 	/**
 	 * Creates a new model.
 	 * If creation is successful, the browser will be redirected to the 'view' page.
@@ -58,18 +63,22 @@ class PecesController extends Controller
 	public function actionCreate()
 	{
 		$model=new Peces;
+
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
+
 		if(isset($_POST['Peces']))
 		{
 			$model->attributes=$_POST['Peces'];
 			if($model->save())
 				$this->redirect(array('view','id'=>$model->id));
 		}
+
 		$this->render('create',array(
-				'model'=>$model,
+			'model'=>$model,
 		));
 	}
+
 	/**
 	 * Updates a particular model.
 	 * If update is successful, the browser will be redirected to the 'view' page.
@@ -78,18 +87,22 @@ class PecesController extends Controller
 	public function actionUpdate($id)
 	{
 		$model=$this->loadModel($id);
+
 		// Uncomment the following line if AJAX validation is needed
 		// $this->performAjaxValidation($model);
+
 		if(isset($_POST['Peces']))
 		{
 			$model->attributes=$_POST['Peces'];
 			if($model->save())
 				$this->redirect(array('view','id'=>$model->id));
 		}
+
 		$this->render('update',array(
-				'model'=>$model,
+			'model'=>$model,
 		));
 	}
+
 	/**
 	 * Deletes a particular model.
 	 * If deletion is successful, the browser will be redirected to the 'admin' page.
@@ -98,35 +111,23 @@ class PecesController extends Controller
 	public function actionDelete($id)
 	{
 		$this->loadModel($id)->delete();
+
 		// if AJAX request (triggered by deletion via admin grid view), we should not redirect the browser
 		if(!isset($_GET['ajax']))
 			$this->redirect(isset($_POST['returnUrl']) ? $_POST['returnUrl'] : array('admin'));
 	}
+
 	/**
 	 * Lists all models.
 	 */
 	public function actionIndex()
 	{
-		
-		$dataProvider=new CActiveDataProvider('Peces', array(
-				'criteria' => array ('order'=>'nombre_comun ASC'),
-		));
-		$this->render('index',array(
-				'dataProvider'=>$dataProvider,
-		));
-		/*
 		$dataProvider=new CActiveDataProvider('Peces');
 		$this->render('index',array(
-				'dataProvider'=>$dataProvider,
-		));*/
+			'dataProvider'=>$dataProvider,
+		));
 	}
-	/**
-	 * La Introduccion de peces
-	 */
-	public function actionInicio()
-	{
-		$this->render('inicio');
-	}
+
 	/**
 	 * Manages all models.
 	 */
@@ -136,273 +137,18 @@ class PecesController extends Controller
 		$model->unsetAttributes();  // clear any default values
 		if(isset($_GET['Peces']))
 			$model->attributes=$_GET['Peces'];
+
 		$this->render('admin',array(
-				'model'=>$model,
+			'model'=>$model,
 		));
 	}
 	
 	/**
-	 * Resulatdo de las busquedas
+	 * La Introduccion de peces
 	 */
-	public function actionResultado()
+	public function actionInicio()
 	{
-		$condiciones=array();
-		$union='';
-		$joins='';
-		$params = $_GET;
-		$select = 'SELECT * FROM peces p ';
-		$order = '';
-		$flag_ficha = false;
-		
-		if(isset($params['especie_id']) && !empty($params['especie_id']))
-		{
-			$flag_ficha = true;
-			array_push($condiciones, "especie_id = ".$params['especie_id']);
-			
-		} else {
-			$flag_ficha = false;
-			
-			if (isset($params['nombre_comun']) && !empty($params['nombre_comun']))
-				array_push($condiciones, "nombre_comun LIKE '%".$params['nombre_comun']."%'");
-			
-			if (isset($params['nombre_cientifico']) && !empty($params['nombre_cientifico']))
-				array_push($condiciones, "nombre_cientifico LIKE '%".$params['nombre_cientifico']."%'");
-			
-			if (isset($params['grupo']) && !empty($params['grupo']))
-				array_push($condiciones, "grupo_id = ".$params['grupo']);
-			
-			//Las zonas varia de 1 a 6
-			if (isset($params['zona']) && ((Int)$params['zona'] > 0 && (Int)$params['zona'] < 7))
-			{
-				$joins.= CartaNacional::join();
-				array_push($condiciones, "cn.Nivel1=".(Int)$params['zona']." AND cn.Nombre != 'Sin datos.'");
-			
-			} elseif (isset($params['zona']) && ((Int)$params['zona'] == 7))  
-			
-			//Caso del importado
-			array_push($condiciones, "(nacional_Importado='Importado' OR nacional_Importado='Nacional e Importado')");		
-			
-			//Caso de una o mas recomendaciones
-			if (isset($params['recomendacion']) && is_array($params['recomendacion']))
-			{
-				$colores = array('V+', 'V-', 'A-', 'A+', 'R');
-				$interseccion = array_intersect($colores, $params['recomendacion']);
-			}
-			
-			if (isset($interseccion) && count($interseccion) > 0)
-			{
-				array_push($condiciones, "recomendacion=1 AND peso_promedio IS NOT NULL AND peso IS NOT NULL");
-				$order.= ' ORDER BY peso_promedio, tipo_imagen, nombre_comun ASC';
-				$recomendaciones = array();
-				
-				foreach ($interseccion as $index => $recomendacion)
-					array_push($recomendaciones, "peso LIKE '%$recomendacion%'");
-				
-				array_push($condiciones, "(".implode(" OR ", $recomendaciones).")");	
-				
-			} else   //busqueda sin recomendacion ni libre, te saca por default todos con recomendacion
-				$order.= ' ORDER BY ISNULL(peso_promedio), peso_promedio, tipo_imagen, nombre_cientifico ASC';
-		}
-		
-		//Para ponerlas como sql
-		if (count($condiciones) > 0) 
-			$condiciones = implode(" AND ", $condiciones); 
-		
-		//decide cual tipo de busqueda es
-		if (!empty($joins))
-		{
-			$resultados=Yii::app()->db->createCommand($select.$joins." WHERE ".$condiciones.$order)->queryAll();
-			$count=Yii::app()->db->createCommand("SELECT COUNT(*) as count FROM peces p ".$joins." WHERE ".$condiciones)->queryAll();
-			$pages = new CPagination($count[0]["count"]);
-			$pages->setPageSize(50);
-		}
-		elseif (!empty($condiciones))
-		{
-			$resultados=Yii::app()->db->createCommand($select." WHERE ".$condiciones.$order)->queryAll();
-			$count=Yii::app()->db->createCommand("SELECT COUNT(*) as count FROM peces p WHERE ".$condiciones)->queryAll();
-			$pages = new CPagination($count[0]["count"]);
-			$pages->setPageSize(50);
-		} else { //para ver todos los peces	
-			$resultados=Yii::app()->db->createCommand($select.$order)->queryAll();
-			$count=Yii::app()->db->createCommand("SELECT COUNT(*) as count FROM peces p")->queryAll();
-			$pages = new CPagination($count[0]["count"]);
-			$pages->setPageSize(50);			
-		}
-
-		if (count($resultados) > 0)  //Si hubo resultados en la busqueda
-		{
-			/*if(isset($params['json']) && !empty($params['json']) && (Int)$params['json']==1)
-			{
-				header('Content-type: application/json; charset=UTF-8');
-				
-				if (isset($params['allow_o']) && $params['allow_o'] == '1')
-				{
-					// Para poder consumir la respuesta del lado del cliente en cualquier servidor, ojo cambiar cuando se tenga el dominio correcto
-					header("Access-Control-Allow-Origin: *");
-					header("Access-Control-Allow-Methods: GET");
-				}	
-				
-				$data = array();
-				$arr_obj = array();
-				
-				foreach($resultados as $k)
-				{
-					$json = array();
-					$pez = Peces::model()->findByPk($k["especie_id"]);
-					
-					if ($pez->tipo_imagen == 1)
-						$pez->imagen = str_replace('index.php/', '', Yii::app()->createAbsoluteUrl('imagenes/peces/'.$pez->imagen));
-					elseif ($pez->tipo_imagen == 2)
-						$pez->imagen = str_replace('index.php/', '', Yii::app()->createAbsoluteUrl('imagenes/siluetas/'.$pez->imagen));
-
-					$json["peces"] = $pez->attributes;
-					
-					\/*
-					//Para la imgen del semaforo
-					if ($pez->recomendacion == 1)
-						$json["peces"]["imagen_semaforo"] = str_replace('index.php/', '', Yii::app()->createAbsoluteUrl('imagenes/semaforo/'.Peces::peso_a_nombre_imagen($pez->peso)));
-					else
-						$json["peces"]["imagen_semaforo"] = "";
-					*\/
-					
-					$json["grupo"] = !empty($pez->grupo)?$pez->grupo->attributes:array();
-					$json["tipo_veda"] = !empty($pez->tipoVeda)?$pez->tipoVeda->attributes:array();
-					
-					if($pez->cartaNacionals)
-					{
-						$aux = array();
-						foreach ($pez->cartaNacionals as $k){
-							array_push($aux, $k->attributes);
-						}
-						$json["carta_nacional"] = $aux; 
-					} 
-					if($pez->distribucions)
-					{
-						$aux = array();
-						foreach ($pez->distribucions as $k){
-							array_push($aux, $k->attributes);
-						}
-						$json["distribucion"] = $aux;
-					}					
-					if($pez->estadoConservacions)
-					{
-						$aux = array();
-						foreach ($pez->estadoConservacions as $k){
-							array_push($aux, $k->attributes);
-						}
-						$json["estado_conservacion"] = $aux;
-					}				
-					if($pez->tipoCapturases)
-					{
-						$aux = array();
-						foreach ($pez->tipoCapturases as $k){
-							array_push($aux, $k->attributes);
-						}
-						$json["tipo_captura"] = $aux;
-					}
-					
-					if(!$flag_ficha)
-						array_push($data, $json);
-					else {
-						echo preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($json));
-						//echo json_encode($json, JSON_UNESCAPED_UNICODE);
-					}
-				}
-				if(!$flag_ficha){
-					echo preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($data));
-					//echo json_encode($data, JSON_UNESCAPED_UNICODE);
-				}	
-			} else */
-					
-				if (isset($params['ajax']) && $params['ajax'] == '1')
-				{
-					$this->renderPartial('_resultado',array(
-							'resultados'=>$resultados,
-							'count'=>$count[0]["count"],
-							'page_size'=>50,
-							'pages'=>$pages
-					));
-				} else { 
-
-					$this->render('resultado',array(
-					'resultados'=>$resultados,
-					'count'=>$count[0]["count"],
-					'page_size'=>50,
-					'pages'=>$pages
-				));
-				}
-		} else
-			$this->renderPartial('_resultado',array('vacio' => '<b>Tu b&uacute;squeda no di&oacute; ning&uacute;n resultado</b>'));
-	}
-	/**
-	 * Guarda o lee los filtros
-	 */
-	public function actionFiltros()
-	{		
-		$params = $_POST;
-		$sesion = Yii::app()->getSession()->getSessionId();
-		$filtro=Filtros::model()->findByAttributes(array('sesion'=>$sesion));
-		if (isset($params['accion']) && $params['accion'] == "guarda")
-		{
-			if (count($filtro) == 1)
-				$filtro_asigno = $this->asignaCampos($params, $filtro);
-			else {
-				$f=new Filtros();
-				$filtro_asigno = $this->asignaCampos($params, $f);
-				$filtro_asigno->sesion = $sesion;
-			}
-			$filtro_asigno->save();
-		} elseif (isset($params['accion']) && $params['accion'] == "leer" && isset($filtro->id)) {
-			echo $filtro->aJSON();
-		} else
-			return NULL;
-	}
-	/**
-	 * Asigna los campos a los filtros indicados
-	 */
-	public function asignaCampos($params, $filtro)
-	{
-		unset($params['accion']);
-		$llaves = array_keys($params);
-		foreach ($llaves as $k => $llave)
-			$filtro->$llave = $params[$llave];
-		return $filtro;
-	}
-	/**
-	 * Borra el registro de los filtros
-	 */
-	public function actionBorrafiltros()
-	{
-		$filtro=Filtros::model()->findByAttributes(array('sesion'=>Yii::app()->getSession()->getSessionId()));
-		if (count($filtro) == 1)
-			$filtro->delete();
-	}
-	
-	public function actionCompleta_pesos()
-	{
-		Peces::model()->completa_pesos();
-		Peces::model()->ordena();
-	}
-	
-	/**
-	 * Para ver el catalogo de grupos
-	 */
-	public function actionGrupos()
-	{
-		header('Content-type: application/json; charset=UTF-8');
-		$resultados = Grupo::model()->findAll();
-		$data = array();
-		$json = array();
-		
-		foreach($resultados as $k)
-		{
-			$grupo = Grupo::model()->findByPk($k["id"]);
-			$json["grupos"] = $grupo->attributes;
-			array_push($data, $json);
-		}
-		
-		//echo json_encode($data, JSON_UNESCAPED_UNICODE);
-		echo preg_replace("/\\\\u([a-f0-9]{4})/e", "iconv('UCS-4LE','UTF-8',pack('V', hexdec('U$1')))", json_encode($data));
+		$this->render('inicio');
 	}
 
 	/**
@@ -419,6 +165,7 @@ class PecesController extends Controller
 			throw new CHttpException(404,'The requested page does not exist.');
 		return $model;
 	}
+
 	/**
 	 * Performs the AJAX validation.
 	 * @param Peces $model the model to be validated
